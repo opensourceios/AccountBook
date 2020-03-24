7e5c1f866ec35a7764b4849098e295c604475d9f
Mason Sun <421680227@qq.com>
2020-03-24 20:55:42 +0800

    Code improvement

--- AccountBook/AccountBook/AmountTextField.swift
+++ AccountBook/AccountBook/AmountTextField.swift
@@ -13,8 +13,8 @@ struct AmountTextField : UIViewRepresentable {
     @Binding var value: String?
 
     func makeCoordinator() -> Coordinator {
-           Coordinator(self)
-       }
+        Coordinator(self)
+    }
 
     func makeUIView(context: Context) -> UITextField {
         let textField = UITextField()
